6a8790821451210f7c15bcb885d6b122ffa0191e
William Towe <willbur1984@gmail.com>
2023-09-23 18:34:34 -0400

    Initial Commit

---
+++ Feige/Feige.h
@@ -0,0 +1,18 @@
+//
+//  Feige.h
+//  Feige
+//
+//  Created by William Towe on 9/23/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Feige.
+FOUNDATION_EXPORT double FeigeVersionNumber;
+
+//! Project version string for Feige.
+FOUNDATION_EXPORT const unsigned char FeigeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Feige/PublicHeader.h>
+
+

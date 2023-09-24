bea8eb314e9c98a48040a81e96c8364d901be6b0
William Towe <willbur1984@gmail.com>
2023-09-24 15:19:13 -0400

    Use fullwidth theme

--- scripts/jazzy.sh
+++ scripts/jazzy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-bundle exec jazzy --clean --author Kosoku --author_url http://kosoku.com/ --source-host github --source-host-url https://github.com/Kosoku/Feige --module-version 1.0.1 --build-tool-arguments -scheme,Feige --module Feige --root-url https://kosoku.github.io/Feige/ --output docs --theme apple
+bundle exec jazzy --clean --author Kosoku --author_url http://kosoku.com/ --source-host github --source-host-url https://github.com/Kosoku/Feige --module-version 1.0.1 --build-tool-arguments -scheme,Feige --module Feige --root-url https://kosoku.github.io/Feige/ --output docs --theme fullwidth

8cc3b0641c2a51d3513406dd6634d368e39c3e50
William Towe <willbur1984@gmail.com>
2023-10-07 19:21:44 -0400

    Remove package dependency

--- Package.swift
+++ Package.swift
@@ -28,10 +28,6 @@ let package = Package(
             name: "Feige",
             targets: ["Feige"]),
     ],
-    dependencies: [
-            // other dependencies
-            .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.3.0"),
-        ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.

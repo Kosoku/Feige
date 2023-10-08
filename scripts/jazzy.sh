fce0abc61e20f3a9f599f61fc2efa125ca994ea6
William Towe <willbur1984@gmail.com>
2023-10-07 22:47:19 -0400

    Update docs

--- scripts/jazzy.sh
+++ scripts/jazzy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-bundle exec jazzy --clean --author Kosoku --author_url http://kosoku.com/ --source-host github --source-host-url https://github.com/Kosoku/Feige --module-version 1.0.1 --build-tool-arguments -scheme,Feige --module Feige --root-url https://kosoku.github.io/Feige/ --output docs --theme fullwidth
+bundle exec jazzy --clean --author Kosoku --author_url http://kosoku.com/ --source-host github --source-host-url https://github.com/Kosoku/Feige --module-version 1.3.0 --build-tool-arguments -scheme,Feige --module Feige --root-url https://kosoku.github.io/Feige/ --output docs --theme fullwidth

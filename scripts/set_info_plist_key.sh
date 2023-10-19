6f6ec88c24e7fbeabbf7dc49e21ff2c15ec235fd
William Towe <willbur1984@gmail.com>
2023-10-19 13:24:40 -0400

    More refactoring

--- scripts/set_info_plist_key.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-PLIST_BUDDY_PATH="/usr/libexec/PlistBuddy"
-INFO_PLIST="Feige/Info.plist"
-
-$PLIST_BUDDY_PATH -c "Set :$1 $2" $INFO_PLIST
--- scripts/set_podspec_version.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-PODSPEC="Feige.podspec"
-
-sed -i "" "s/\(s.version = '\).*\('\)/\1$1\2/" $PODSPEC
